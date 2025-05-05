13c2f211317674be60ad28ca0b12a5874d4ea690
Godwill Kiplagat <godwillkiprop@gmail.com>
2025-05-05 13:52:59 +0300

    updated the index.html page for the new/latest items added to be displayed on the new arrivals

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -8,6 +9,25 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
         ordering = ('name',)
        
+
+    def __str__(self):
+        return self.name
+    
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='item_images/', blank=True, null=True)
+    is_available = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, related_name = 'items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+   
+    
+    class Meta:
+        ordering = ('name',)
 
     def __str__(self):
         return self.name
