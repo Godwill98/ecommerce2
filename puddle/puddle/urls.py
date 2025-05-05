899a6625836b38c8effcacdc16396ee37e905d04
Godwill Kiplagat <godwillkiprop@gmail.com>
2025-05-05 14:19:25 +0300

    created urls.py for puddle

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, contact  # Import the views from the core app
 
 urlpatterns = [
     
     path('', index, name='index'),
+    path('items/', include('item.urls')),  # Include item app URLs with namespace
     path('contact/',contact, name ='contact'),
     path('admin/', admin.site.urls),
      # Add this line to include the index view
